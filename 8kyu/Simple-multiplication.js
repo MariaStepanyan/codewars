2f6a54c35258f25ab5dc088d3650d2b540e7bf1f
MariaStepanyan <drmariastepanyan@gmail.com>
2023-11-13 16:06:45 +0300

    finish even number

--- 8kyu/Simple-multiplication.js
+++ 8kyu/Simple-multiplication.js
@@ -1,8 +1,14 @@
 // DESCRIPTION:
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-//SOLUTION
+// SOLUTION
 
 function simpleMultiplication(number) {
   return number % 2 === 0 ? number * 8 : number * 9
+}
+
+// SOLUTION 2
+
+function simpleMultiplication(number) {
+  return number * (number % 2 ? 9 : 8)
 }
