f8e827ed8ee7f3de6d747212f7896894abda01eb
MariaStepanyan <drmariastepanyan@gmail.com>
2023-11-15 16:41:36 +0300

    reverce

---
+++ 8kyu/Reversed-sequence.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// SOLUTION
+
+const reverseSeq = (n) => {
+  let arr = []
+
+  for (let i = 0; i < n; i++) {
+    arr.push(n - i)
+  }
+  return arr
+}
+
+// SOLUTIION 2
+
+const reverseSeq2 = (n) => Array.from({ n }, () => n--)
