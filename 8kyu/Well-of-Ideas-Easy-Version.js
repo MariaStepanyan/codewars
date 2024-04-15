75292274ff9ddbe3775aeaeba6580fda0683ed85
MariaStepanyan <drmariastepanyan@gmail.com>
2024-04-15 11:14:42 +0300

    well of ideas easy version

---
+++ 8kyu/Well-of-Ideas-Easy-Version.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x) {
+  const filteredArr = x.filter((el) => el === 'good')
+  return filteredArr.length === 0
+    ? 'Fail!'
+    : filteredArr.length > 2
+    ? 'I smell a series!'
+    : 'Publish!'
+}
+
+console.log(well(['good', 'good', 'good', 'bad', 'bad', 'bad', 'bad']))
